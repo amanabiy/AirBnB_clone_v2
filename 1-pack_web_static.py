bfc7cf5a24a1d44a1ec986c38ee6631eca37785b
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-26 22:46:44 +0300

    fab

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# prepare tar file
+from fabric.api import local
+from time import strftime
+
+
+def do_pack():
+    """generate .tgz archive"""
+    timenow = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        filename = "versions/web_static_{}.tgz".format(timenow)
+        local("tar -cvzf {} web_static/".format(filename))
+        return filename
+    except:
+        return None
