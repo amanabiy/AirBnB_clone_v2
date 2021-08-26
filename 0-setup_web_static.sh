d7b0169bbfcf5001dcaaa5c35569fb43a850dd03
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-08-26 22:33:26 +0300

    configure nginx to deploy a static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# sets up web server for static web deployment
+HBNB="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "Works perfectly" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i "35i $HBNB" /etc/nginx/sites-available/default
+sudo service nginx start
