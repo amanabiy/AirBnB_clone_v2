c570700b84ff4bfc11dde205841cea640197501d
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-09-12 23:35:36 +0300

    a function with the given data

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Flask with the following routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Say hello """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ say hnbn """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ Displays C plus the text entered """
+    return "C {}".format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
